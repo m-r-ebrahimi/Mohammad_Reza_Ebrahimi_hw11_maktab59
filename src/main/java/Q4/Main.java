819f50d38f015653752cdcbccb8d78ebc72c4096
m-r-ebrahimi <emohammadreza568@gmail.com>
2021-10-19 12:32:03 +0330

    Complete Main.

--- src/main/java/Q4/Main.java
+++ src/main/java/Q4/Main.java
@@ -1,4 +1,25 @@
 package Q4;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        List<Book> list= new LinkedList<>();
+
+        Thread[] threads = {
+                new Thread(new Producer(list)),new Thread(new Consumer(list)),
+                new Thread(new Producer(list)),new Thread(new Consumer(list))
+        };
+
+        for(Thread thread:threads){
+            thread.start();
+        }
+
+        for(Thread thread:threads){
+            thread.join();
+        }
+
+        System.out.println("finished, final size: "+list.size());
+    }
 }
